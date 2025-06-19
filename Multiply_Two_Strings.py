4e9b8da29ad1087efa40851cc0347ad04f2512de
Krishn1101 <agrawalkrishna031@gmail.com>
2025-06-19 23:38:05 +0530

    Create Multiply_Two_Strings.py

---
+++ Multiply_Two_Strings.py
@@ -0,0 +1,23 @@
+"""Given two numbers as strings s1 and s2. Calculate their Product.
+Note: The numbers can be negative and You are not allowed to use any built-in function or convert the strings to integers. There can be zeros in the begining of the numbers. You don't need to specify '+' sign in the begining of positive numbers.
+
+Examples:
+
+Input: s1 = "0033", s2 = "2"
+Output: "66"
+Explanation: 33 * 2 = 66
+Input: s1 = "11", s2 = "23"
+Output: "253"
+Explanation: 11 * 23  = 253
+Input: s1 = "123", s2 = "0"
+Output: "0"
+Explanation: Anything multiplied by 0 is equal to 0."""
+
+                                                        #CODE HERE:-
+
+class Solution:
+    def multiplyStrings(self, s1, s2):
+        s1 = int(s1)
+        s2 = int(s2)
+        s3 = s1 * s2
+        return s3
