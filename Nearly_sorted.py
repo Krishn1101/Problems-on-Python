4b7e0824729f61a94d0077e0f65dda33fd9d3d54
Krishn1101 <agrawalkrishna031@gmail.com>
2025-07-08 22:08:47 +0530

    Create Nearly_sorted.py

---
+++ Nearly_sorted.py
@@ -0,0 +1,17 @@
+"""Given an array arr[], where each element is at most k away from its target position, you need to sort the array optimally.
+Note: You need to change the given array arr[] in place.
+
+Examples:
+
+Input: arr[] = [6, 5, 3, 2, 8, 10, 9], k = 3
+Output: [2, 3, 5, 6, 8, 9, 10]
+Explanation: The sorted array will be 2 3 5 6 8 9 10
+Input: arr[]= [1, 4, 5, 2, 3, 6, 7, 8, 9, 10], k = 2
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Explanation: The sorted array will be 1 2 3 4 5 6 7 8 9 10"""
+
+                                                        #CODE HERE:-
+
+class Solution:
+    def nearlySorted(self, arr, k):
+        return(arr.sort())
