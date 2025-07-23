165ef1bfd3c8f9a6fd2063bb0547fa6aec40f691
Krishn1101 <agrawalkrishna031@gmail.com>
2025-07-23 22:30:53 +0530

    Create Swap_Two_Numbers.py

---
+++ Swap_Two_Numbers.py
@@ -0,0 +1,18 @@
+"""You are given two numbers a and b. Your task is to swap the given two numbers.
+
+Note: Try to do it without a temporary variable.
+
+Examples:
+
+Input: a = 13, b = 9
+Output: 9 13
+Explanation: After swapping it becomes 9 and 13.
+Input: a = 15, b = 8
+Output: 8 15
+Explanation: after swapping it becomes 8 and 15."""
+
+                                #CODE HERE:-
+
+class Solution:
+    def get(self, a: int, b: int) -> tuple[int, int]:
+        return(b,a)
