4e167bb782167adcfe0967c20dda6f7db6dde4d0
Krishn1101 <agrawalkrishna031@gmail.com>
2025-08-04 23:37:05 +0530

    Create Binary_Modulo.py

---
+++ Binary_Modulo.py
@@ -0,0 +1,29 @@
+"""You are given a binary string s and an integer m. You need to return an integer r. Where r = k%m, k is the binary equivalent of string s.
+
+Example 1:
+
+Input:
+s = "101" 
+m = 2
+Output:
+1
+Explanation: Here k=5 because (101)2 = (5)10.
+Hence 5 mod 2 = 1.
+Example 2:
+
+Input:
+s = "1000"
+m = 4
+Output:
+0
+Explanation: Here k=8 and m=4 hence 
+r = k mod m = 8 mod 4 = 0."""
+
+                                                            #CODE HERE:-
+
+#User function Template for python3
+
+class Solution():
+    def modulo(self, s, m):
+        k = int(s, 2)
+        return(k%m)
