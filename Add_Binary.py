15e07a383c970f4f108ccad578ee9d5bca25e95b
Krishn1101 <agrawalkrishna031@gmail.com>
2025-08-18 00:02:11 +0530

    Create Add_Binary.py

---
+++ Add_Binary.py
@@ -0,0 +1,21 @@
+"""Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"""
+
+                            #CODE HERE:-
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a = int(a,2)
+        b = int(b,2)
+        ans = a + b
+        return (bin(ans)[2:])
