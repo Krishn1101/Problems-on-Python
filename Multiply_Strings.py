b0ef31d96a211cc2809c6f73c22bb10199d13763
Krishn1101 <agrawalkrishna031@gmail.com>
2025-08-18 00:02:08 +0530

    Create Multiply_Strings.py

---
+++ Multiply_Strings.py
@@ -0,0 +1,23 @@
+"""Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+ 
+
+Example 1:
+
+Input: num1 = "2", num2 = "3"
+Output: "6"
+Example 2:
+
+Input: num1 = "123", num2 = "456"
+Output: "56088"""
+
+                                                            #CODE HERE:-
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        num1 = int(num1)
+        num2 = int(num2)
+        ans = str(num1*num2)
+        return ans
