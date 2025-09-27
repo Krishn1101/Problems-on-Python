41ec2515561b1636e1074c0c4df75c116a20ba90
Krishn1101 <agrawalkrishna031@gmail.com>
2025-09-27 11:25:05 +0530

    Create Greatest_Of_Three.py

---
+++ Greatest_Of_Three.py
@@ -0,0 +1,17 @@
+"""Given three numbers a, b, and c. You need to find which is the greatest of them all.
+
+Examples:
+
+Input: a = 1, b = 2, c = 3
+Output: 3
+Explanation: Clearly, c = 3 is the greatest of (1, 2, 3)
+Input: a = 2, b = 2, c = 5
+Output: 5
+Explanation: Out of (2, 2, 5) 5 is the greatest."""
+
+                                            #CODE HERE:-
+
+a = int(input())
+b = int(input())
+c = int(input())
+print(max(a,b,c))
