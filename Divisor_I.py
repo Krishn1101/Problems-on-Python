a8454c4a79d6897ec867c89380fadd2dd4eaf0a7
Krishn1101 <agrawalkrishna031@gmail.com>
2025-09-29 13:55:28 +0530

    Create Divisor_I.py

---
+++ Divisor_I.py
@@ -0,0 +1,18 @@
+"""Given an integer n. Write a program to print all the divisors of n in a single line.
+
+Examples:
+
+Input: n = 12
+Output: 1 2 3 4 6 12
+Explanation: 12 is divisible by 1 2 3 4 6 12.
+Input: n = 10
+Output: 1 2 5 10
+Explanation: 10 is divisible by 1 2 5 10."""
+
+                                        #CODE HERE:-
+
+class Solution:
+    def print_divisors(self, n):
+        for i in range(1,n+1):
+            if n%i==0:
+                print(i, end = " ")
