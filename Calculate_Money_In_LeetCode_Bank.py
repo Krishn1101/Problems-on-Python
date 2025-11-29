237381389dd4745510cafb86f4e5905360564b8b
Krishn1101 <agrawalkrishna031@gmail.com>
2025-11-29 20:58:19 +0530

    Create Calculate_Money_In_LeetCode_Bank.py

---
+++ Calculate_Money_In_LeetCode_Bank.py
@@ -0,0 +1,45 @@
+"""Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+
+He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+
+Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+ 
+
+Example 1:
+
+Input: n = 4
+Output: 10
+Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+Example 2:
+
+Input: n = 10
+Output: 37
+Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+Example 3:
+
+Input: n = 20
+Output: 96
+Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96."""
+
+                                                                #CODE HERE:-
+
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        sm = 0
+        cnt = 0
+        m = 0
+        if n<=7:
+            for i in range(1,n+1):
+                sm+=i
+                cnt+=1
+            return sm
+        else:
+            while(cnt<n):
+                m+=1
+                for i in range(m,m+7):
+                    sm+=i
+                    cnt+=1
+                    if cnt==n:
+                        break
+            return sm
