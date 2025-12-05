75e3c2fe22b20fa71ae6ed05f0379420202fbd9b
Krishn1101 <agrawalkrishna031@gmail.com>
2025-12-05 23:41:19 +0530

    Create Remove_Duplicates_From_Sorted_List.py

---
+++ Remove_Duplicates_From_Sorted_List.py
@@ -0,0 +1,37 @@
+"""Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]"""
+
+                                                            #CODE HERE:-
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head==None or head.next == None:
+            return head
+        else:
+            curr = head
+            while curr!=None:
+                if curr.next!=None and curr.next.val == curr.val:
+                    curr.next = curr.next.next
+                else:
+                    curr = curr.next
+            return head
