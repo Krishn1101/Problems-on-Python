bdb11ce2f3a1bc9cf6eb1586c3d4bc47b4a6a1d0
Krishn1101 <agrawalkrishna031@gmail.com>
2026-01-15 22:30:42 +0530

    Create Length_Of_String.py

---
+++ Length_Of_String.py
@@ -0,0 +1,14 @@
+"""You are given a string s. You need to find the length of the string and return it.
+
+Examples:
+
+Input: s = Geeks
+Output: 5
+Input: s = Hello1234
+Output: 9"""
+
+                                        #CODE HERE:-
+
+class Solution:
+    def lengthString(self, s):
+        return len(s)
