da11ce9f4b3f4dbcda75c369fbc6ebf0a6fb2f2d
Krishn1101 <agrawalkrishna031@gmail.com>
2026-01-15 22:30:46 +0530

    Create Lower_Case_To_Upper_Case.py

---
+++ Lower_Case_To_Upper_Case.py
@@ -0,0 +1,18 @@
+"""Given a string str containing only lowercase letters, generate a string with the same letters, but in uppercase.
+
+Example 1:
+
+Input:
+str = "geeks"
+Output: GEEKS
+Example 2:
+
+Input:
+str = "geeksforgeeks"
+Output: GEEKSFORGEEKS"""
+
+                                                    #CODE HERE:-
+
+class Solution:
+    def to_upper(self, s):
+        return s.upper()
