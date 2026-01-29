919f14c5d871e957bd74dac83966645066be3019
Krishn1101 <agrawalkrishna031@gmail.com>
2026-01-29 22:27:09 +0530

    Create String_Duplicates_Removal.py

---
+++ String_Duplicates_Removal.py
@@ -0,0 +1,22 @@
+"""Given a string s which may contain lowercase and uppercase characters. The task is to remove all duplicate characters from the string and find the resultant string. The order of remaining characters in the output should be same as in the original string.
+
+Example 1:
+
+Input: s = "geEksforGEeks"
+Output: "geEksforG"
+Explanation: After removing duplicate characters such as E, e, k, s, we have string as "geEksforG".
+Example 2:
+
+Input: s = "HaPpyNewYear"
+Output: "HaPpyNewYr"
+Explanation: After removing duplicate characters such as e, a, we have string as "HaPpyNewYr"."""
+
+                                                            #CODE HERE:-
+
+class Solution:
+    def removeDuplicates(self, s):
+        ans = ""
+        for i in s:
+            if i not in ans:
+                ans+=i
+        return ans
