21e44b0f48cd8b6111f88bc872578ce62bb413b8
Krishn1101 <agrawalkrishna031@gmail.com>
2026-02-09 15:35:01 +0530

    Create Insertion_In_Deque.py

---
+++ Insertion_In_Deque.py
@@ -0,0 +1,16 @@
+"""Given an array arr[] containing non-negative integers. You need to insert all elements of the array to deque and return it.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: [1, 2, 3, 4, 5]
+Explanation: After insert in the deque it will look like [1, 2, 3, 4, 5].
+Input: arr[] = [1]
+Output: [1]
+Explanation: After insert in the deque it will look like [1]."""
+
+                                                            #CODE HERE:-
+
+class Solution:
+    def dqInsertion(self, arr):
+        return arr
