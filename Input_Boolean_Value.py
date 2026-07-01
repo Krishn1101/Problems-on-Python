89302c8d82996eb9a7c45fc89db8c0fdcf3c7120
Krishn1101 <agrawalkrishna031@gmail.com>
2026-07-01 21:55:21 +0530

    Create Input_Boolean_Value.py

---
+++ Input_Boolean_Value.py
@@ -0,0 +1,18 @@
+"""Take a boolean value a as an input. The program should display its numeric representation:
+
+1 for "true"
+0 for "false"
+Examples: 
+
+Input: a = true
+Output: 1
+Input: a = false
+Output: 0"""
+
+                                            #CODE HERE:-
+
+a = input()
+if a == "true":
+    print(1)
+elif a == "false":
+    print(0)
