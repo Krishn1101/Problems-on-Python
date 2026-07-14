464887a2e2a192ae5f5732dedf422d91e2ce72d2
Krishn1101 <agrawalkrishna031@gmail.com>
2026-07-14 12:04:55 +0530

    Create Static_Method_In_Python.py

---
+++ Static_Method_In_Python.py
@@ -0,0 +1,25 @@
+"""Design a class as described below:
+
+Class Name: Addition
+Method:
+Function Name: add
+Parameters: a (int), b (int)
+Return Type: int
+Static: Yes (Use the @staticmethod decorator)
+Task: Returns the sum of the values given as parameters.
+Examples:
+
+Input: a = 3, b = 4
+Output: 7"""
+
+                #CODE HERE:-
+
+# Implement Addition class here
+class Addition:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def add(a,b):
+        return a + b
+    
